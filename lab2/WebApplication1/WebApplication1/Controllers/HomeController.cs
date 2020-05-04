45aba1f7ac07dac074f64cb00928cbd71f080cc3
salsaaa <salsafci97@gmail.com>
2020-05-04 16:12:16 +0200

    lab2

---
+++ lab2/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RegisterForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RegisterForm(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = new Users();
+                user.Employees.Add(employee);
+                user.SaveChanges();
+                TempData["Name"] = employee.Name;
+                return View("Welcome", employee);
+            }
+            return View();
+        }
+
+    }
+}
